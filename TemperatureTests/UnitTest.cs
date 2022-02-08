53dc2efc915b05fe9eeabd97e6cac96eeee2839c
Ryan Collicutt <ryancollicutt22@gmail.com>
2022-02-07 21:47:44 -0800

    BREAKING TEST CASE ON PURPOSE

--- TemperatureTests/UnitTest.cs
+++ TemperatureTests/UnitTest.cs
@@ -10,7 +10,7 @@ namespace TemperatureTests
         public void Given0CelciusOutput32Fahrenheit()
         {
             Conversion conversion = new Conversion();
-            double expected = 32;
+            double expected = 31;
             double actual = conversion.Convert(Conversion.ConversionMode.Celsius_to_Fahrenheit, 0);
             Assert.Equal(expected, actual);
         }
